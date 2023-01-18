2fca2eb4624bdf5384a06d62944e3e1fd6b67cdd
Evelyne E. Dresner <eedresner@knox.edu>
2023-01-18 14:07:12 -0600

    added mouse tracking

---
+++ Assets/pewPew.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class pewPew : MonoBehaviour
+{
+    public GameObject projectile;
+    public float launchVelocity = 3000;
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Unity Forum Go Crazy
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg-180;
+            transform.rotation = Quaternion.Euler(0, rotation, 0);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            shoot();
+        }
+    }
+
+    private void shoot()
+    {
+        GameObject ball = Instantiate(projectile, transform.position,transform.rotation);
+        ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, launchVelocity, 0));
+
+        Destroy(ball, 3f);
+
+    }
+}
