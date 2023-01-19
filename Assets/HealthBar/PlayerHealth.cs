95c0358ba37aea7a219fdf33305acf4557298ca7
OscarGardella <ohgardella@knox.edu>
2023-01-18 18:39:39 -0600

    Added health bar with basic scripts. Must add it directly to a Canvas.

---
+++ Assets/HealthBar/PlayerHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    
+    public int max_health = 5;
+    public int current_health;
+
+    public HealthBar player_health_bar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_health = max_health;
+        player_health_bar.SetMaxHealth(max_health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(1);
+        }
+    } 
+
+    void TakeDamage(int damage_taken)
+    {
+        current_health -= damage_taken;
+        player_health_bar.SetHealth(current_health);
+    }
+}
