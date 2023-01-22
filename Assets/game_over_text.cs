21706c125344707375741f12458dfd5432ed03da
ch1h0kd <cokada@knox.edu>
2023-01-22 14:45:51 -0600

    Second try, added everything_environment

---
+++ Assets/game_over_text.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class game_over_text : MonoBehaviour
+{
+
+    [SerializeField] Text message;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
