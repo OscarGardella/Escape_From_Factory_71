f1af21d01250ed36283eddad78a62ea627acc96f
OscarGardella <ohgardella@knox.edu>
2023-02-04 22:19:15 -0600

    Saving work on ability pick up screen

--- Assets/Files/Code/Upgrade_Choice.cs
+++ Assets/Files/Code/Upgrade_Choice.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Upgrade_Choice : MonoBehaviour
 {
-    public Image oldSprite;
-    public Image newSprite;
+    public Image abilityIcon;
+    public static Sprite tempSprite;
     
+    class upgradePanel 
+    {
+        string name;
+        string description;
+        int cost;
+        Sprite icon;
+
+        upgradePanel()
+        {
+            name = "Name";
+            description = "This is a description";
+            cost = 0;
+            icon = tempSprite; //"UI_Skill_Icon_Dash";
+        }
+
+        upgradePanel(string myName, string myDescription, int myCost, Sprite myIcon)
+        {
+            name = myName;
+            description = myDescription;
+            cost = myCost;
+            icon = myIcon;
+        }
+
+    }
     public void ChangeSprite()
     {
-        oldSprite.sprite = newSprite; 
+        abilityIcon.sprite = upgradePanel().icon;
     }
 
     // Start is called before the first frame update
