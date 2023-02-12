dc9c0675999c39dc7ef1990b999c308eb4f541fe
ch1h0kd <cokada@knox.edu>
2023-02-12 16:01:15 -0600

    Get SFX bar work

--- Assets/SFXSoundBar.cs
+++ Assets/SFXSoundBar.cs
@@ -1,10 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SFXSoundBar : MonoBehaviour
 {
-    private void playSFX()
+    void Start()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        //entry.eventID = EventTriggerType.Drag;
+        entry.eventID = EventTriggerType.PointerClick;
+        //entry.callback.AddListener(data => { OnDrag((PointerEventData)data); });
+        entry.callback.AddListener(data => { OnPointerClick((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        //Debug.Log("Mouse is over GameObject");
+        AudioManager.Instance.PlaySFX("Player Shooting");
+    }
+
+    public void OnPointerClick(PointerEventData data)
     {
         AudioManager.Instance.PlaySFX("Player Shooting");
     }
