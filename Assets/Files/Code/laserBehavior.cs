e31bda3455fb541c56680a54d65cb8c7c0522702
Evelyne E. Dresner <eedresner@knox.edu>
2023-02-18 17:07:41 -0600

    made pewpew and laser behavior work a little cleaner

---
+++ Assets/Files/Code/laserBehavior.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private ScoreKeeper score;
+
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Physics.IgnoreCollision(player.GetComponent<Collider>(), gameObject.GetComponent<Collider>(), true);
+        Destroy(gameObject, 3);
+        score = GameObject.FindGameObjectWithTag("ScoreDisplay").GetComponent<ScoreKeeper>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            score.incrScore(100);
+            Explode();
+        }
+        else if (collision.gameObject.tag != "Player")
+        {
+
+            // Destroy(collision.gameObject);
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        //var exp = GetComponent<ParticleSystem>();
+        //exp.Play();
+        Destroy(gameObject);
+    }
+}
