a57a1792cd705cd937ef0a24ba53feb50ad07e3c
ch1h0kd <cokada@knox.edu>
2023-02-19 09:52:11 -0600

    Delete extra lines

--- Assets/ScoreResult.cs
+++ Assets/ScoreResult.cs
@@ -6,25 +6,20 @@ using TMPro;
 
 public class ScoreResult : MonoBehaviour
 {
-    //public TMP_Text textMesh;
-    public ScoreKeeper sk;
-    public static int score;
-    public Text TextScoreUI;
+    ScoreKeeper sk;
+    int score;
+    Text TextScoreUI;
 
     void Start()
     {
-        TextScoreUI = GameObject.Find("Score Text").GetComponent<Text>();  // if you want to reference it by code - tag it if you have several texts
+        TextScoreUI = GameObject.Find("Score Text").GetComponent<Text>(); 
         sk = GameObject.Find("ScoreDisplay").GetComponent<ScoreKeeper>();
-        if (!TextScoreUI) Debug.Log("ScoreKeeper.cs: Error: Failure retrieving TextScoreUI component. Make sure this script is attached to a TextMesh.");
-        if (!sk) Debug.Log("ScoreKeeper.cs: Error: Failure retrieving sk component. Make sure this script is attached to a TextMesh.");
     }
 
     // Start is called before the first frame update
     void Update()
     {
-        //textMesh = GetComponent<TMP_Text>();
         score = sk.getScore();
         TextScoreUI.text = score.ToString();
-        //TextScoreUI.text = "aaa";
     }
 }
